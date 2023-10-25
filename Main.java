b14f82993f9a1fc149c3cd3689127e7fa897808e
yaniil <yanilezeki@gmail.com>
2023-10-25 23:15:48 +0200

    JUGURTHA

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ public static void main(String[]args){
     System.out.println("EHEHEHE");
     System.out.println("EHEHEHE");
     System.out.println("EHEHEHE");
+    System.out.println("EHEHEHE");
+
 
 
 
